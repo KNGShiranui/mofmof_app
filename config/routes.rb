9733984ec370e35a810aeb66f6b3350ae6009779
kengo <mathematics9973@gmail.com>
2023-02-16 01:11:20 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'rentals#index'
+  resources :properties do
+    collection do
+      post :confirm
+    end
+  end
+  resources :nearest_stations
 end
